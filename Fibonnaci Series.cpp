ecdccb81a7bce52560670db0ee5daf7ae97b1abd
Piyush-123456 <134592487+Piyush-123456@users.noreply.github.com>
2024-06-24 21:51:44 +0530

    Fibonnaci Series

--- Fibonnaci Series.cpp
+++ Fibonnaci Series.cpp
@@ -36,6 +36,20 @@ public:
          }
          return dp[n];
     }
+     int solveusingspaceoptimization(int n) {
+            int ans = 0;
+            int prev = 0;
+            int curr = 1;
+            if (n == 0 || n == 1) {
+                return n;
+            }
+            for (int i = 2; i <= n; i++) {
+                ans = prev + curr;
+                prev = curr;
+                curr = ans;
+            }
+            return ans;
+        }
     int fib(int n) {
         vector<int> dp(n + 1, -1);
         // return solveusingRecursion(n);
